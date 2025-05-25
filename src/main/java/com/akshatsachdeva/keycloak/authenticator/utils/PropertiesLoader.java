13a6c5163f3dd01ca8e648848fa91a6e02bdba98
Nasser Al-Jbreen <n.aljebreen@jeel.co>
2025-05-25 12:35:35 +0300

    added hamc key to be used for validating the webhook along with properties loader and utils to calculate the signature

---
+++ src/main/java/com/akshatsachdeva/keycloak/authenticator/utils/PropertiesLoader.java
@@ -0,0 +1,14 @@
+package com.akshatsachdeva.keycloak.authenticator.utils;
+
+import io.smallrye.config.SmallRyeConfig;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+public class PropertiesLoader {
+    private static final SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+
+    public static final String HMAC_KEY = config.getValue("hmac-key", String.class);
+
+    public static String getProperty(String key) {
+        return config.getValue(key, String.class);
+    }
+}
